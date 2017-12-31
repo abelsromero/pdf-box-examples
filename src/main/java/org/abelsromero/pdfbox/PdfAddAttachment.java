fe2b66d1e707c6bc2642177fd6af4608cd3bda2a
Abel Salgado Romero <abelromero@gmail.com>
2017-12-31 20:45:26 +0100

    refactored and added examples to PdfAddAttachment

--- src/main/java/org/abelsromero/pdfbox/PdfAddAttachment.java
+++ src/main/java/org/abelsromero/pdfbox/PdfAddAttachment.java
@@ -7,9 +7,9 @@ import org.apache.pdfbox.pdmodel.common.filespecification.PDComplexFileSpecifica
 import org.apache.pdfbox.pdmodel.common.filespecification.PDEmbeddedFile;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.util.GregorianCalendar;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -18,37 +18,43 @@ import java.util.Map;
  */
 public class PdfAddAttachment {
 
-    public static final String SRC_FILE = "Test Document 1.pdf";
+    public static final String SRC_PDF_FILE = "simple-pdf.pdf";
+    public static final String SRC_ATACHMENT_FILE = "sample.adoc";
 
     public static void main(String[] args) throws IOException {
 
-        PDDocument doc = PDDocument.load(new File("src/main/resources", SRC_FILE));
+        final PDDocument doc = PDDocument.load(new File("src/main/resources", SRC_PDF_FILE));
 
-        PDEmbeddedFilesNameTreeNode efTree = new PDEmbeddedFilesNameTreeNode();
-
-        // first create the file specification, which holds the embedded file
-        PDComplexFileSpecification fs = new PDComplexFileSpecification();
-        fs.setFile("Test.pdf");
-        InputStream is = new PdfAddAttachment().getClass().getClassLoader().getResourceAsStream(SRC_FILE);
-
-        PDEmbeddedFile ef = new PDEmbeddedFile(doc, is);
+        // Create EmbeddedFile object (/Type /EmbeddedFile) with the actual content
+        final File attachment = new File("src/main/resources", SRC_ATACHMENT_FILE);
+        final PDEmbeddedFile ef = new PDEmbeddedFile(doc, new FileInputStream(attachment));
         // set some of the attributes of the embedded file
-        ef.setSubtype("test/plain");
-        ef.setSize(130759);
-        ef.setCreationDate(new GregorianCalendar());
+        ef.setSubtype("text/plain");
+        ef.setSize((int) attachment.length());
+        ef.setCreationDate(Calendar.getInstance());
+
+        // Create the file specification (/Type /Filespec /F .. /EF), which holds the embedded file
+        final PDComplexFileSpecification fs = new PDComplexFileSpecification();
+        fs.setFile("sample.adoc");
         fs.setEmbeddedFile(ef);
 
-        // now add the entry to the embedded file tree and set in the document.
+        final PDComplexFileSpecification fs2 = new PDComplexFileSpecification();
+        fs2.setFile("sample2.adoc");
+        fs2.setEmbeddedFile(ef);
+
+        // Add the entry to the embedded file tree and set in the document.
         Map efMap = new HashMap();
         efMap.put("custom_attachment", fs);
+        efMap.put("custom_attachment2", fs2);
+        final PDEmbeddedFilesNameTreeNode efTree = new PDEmbeddedFilesNameTreeNode();
         efTree.setNames(efMap);
 
-        // attachments are stored as part of the "names" dictionary in the document catalog
-        PDDocumentNameDictionary names = new PDDocumentNameDictionary(doc.getDocumentCatalog());
+        // Attachments are stored as part of the "/Names" dictionary in the document /Catalog
+        final PDDocumentNameDictionary names = new PDDocumentNameDictionary(doc.getDocumentCatalog());
         names.setEmbeddedFiles(efTree);
         doc.getDocumentCatalog().setNames(names);
 
-        doc.save(new File("output.pdf"));
+        doc.save(new File("pdf-with-attachment.pdf"));
 
     }
 
