09a066fe423c891240bc9eed6153ae91dd5ceeb2
Abel Salgado Romero <abelromero@gmail.com>
2023-02-04 20:26:22 +0100

    Add rotate Left operation and cleanup PdfRotator (#4)

--- src/test/java/org/abelsromero/pdfbox/PdfRotatorTest.java
+++ src/test/java/org/abelsromero/pdfbox/PdfRotatorTest.java
@@ -20,10 +20,26 @@ public class PdfRotatorTest {
         File outputDir = createTestDirectory();
         File file = new File("src/test/resources", SRC_PDF_FILE);
 
-        PdfRotator.Builder.loadPdf(file)
+        PdfRotator.loadPdf(file)
             .rotateRight()
             .writeTo(new File(outputDir, "output.pdf"));
 
+        assertNewSize(outputDir, 843, 596);
+    }
+
+    @Test
+    void should_rotate_pages_to_left() throws IOException {
+        File outputDir = createTestDirectory();
+        File file = new File("src/test/resources", SRC_PDF_FILE);
+
+        PdfRotator.loadPdf(file)
+            .rotateLeft()
+            .writeTo(new File(outputDir, "output.pdf"));
+
+        assertNewSize(outputDir, 842, 596);
+    }
+
+    private static void assertNewSize(File outputDir, int width, int height) throws IOException {
         File output = new File(outputDir, "output.pdf");
         assertThat(output).exists();
         assertThat(PDDocument.load(output).getPages())
@@ -32,8 +48,8 @@ public class PdfRotatorTest {
                 // not 100% accurate test. The actual content does not have a way to get size
                 PDRectangle cropBox = page.getCropBox();
                 // horizontal layout (width > height)
-                assertThat(cropBox.getWidth()).isEqualTo(843);
-                assertThat(cropBox.getHeight()).isEqualTo(596);
+                assertThat(cropBox.getWidth()).isEqualTo(width);
+                assertThat(cropBox.getHeight()).isEqualTo(height);
             });
     }
 
